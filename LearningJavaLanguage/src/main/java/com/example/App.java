8f60ceb1be766bd3413b60fe41b8183c73c5ad45
Carolina <carol18becerra@gmail.com>
2026-04-08 14:58:08 +0200

    modificadores acceso incialmente explicados

--- LearningJavaLanguage/src/main/java/com/example/App.java
+++ LearningJavaLanguage/src/main/java/com/example/App.java
@@ -1,26 +1,13 @@
 package com.example;
 
 public class App {
-    public static void main(String[] args) {
-        System.out.println();
-    
-   // creacion  de objetos Persona, a partir de la clase Persona
-    
-    Persona persona1 = new Persona();
-    
-    persona1.nombre = "Dani";
-    
-    Persona persona2 = new Persona();
-    
-    persona2.nombre = "Dani";
-    
-    /*persona1 y persona2 son iguales*/
-    if (persona1 == persona2)
-    	System.out.println("persona1 y persona2 apuntan al mismo abjeto");
-    else {
-		System.out.println("persona1 y persona2 son variables de referencias"
-						+ "que apuntan a objetos diferentes en la memoria RAM");
+	public static void main(String[] args) {
+
+		Persona persona1 = new Persona();
+
+		persona1.setNombre("Dani");
+		
+		System.out.println("el valor de la variable" + "nombre para la referencia es: "
+		+ persona1.getNombre());
 	}
-    
-    }
 }
--- LearningJavaLanguage/src/main/java/com/example/Persona.java
+++ LearningJavaLanguage/src/main/java/com/example/Persona.java
@@ -2,12 +2,58 @@ package com.example;
 
 public class Persona {
 
+	/*
+	 * A continucion las variables mienbros de la clase, que se le llama:
+	 * propiadades, campos y atributos
+	 */
+
+	/*
+	 * Modificadores de acceso:
+	 * 
+	 * 1- privado del paquete, es el por defecto 2- public => es accesible en todos
+	 * los paquetes 3- private => es solo accesible por elementos de la misma clase,
+	 * dentro de la propia clase y incluso un objeto o una instancia de la misma
+	 * clase no puede accerder a las propiedades privadas 4- protected => herencia,
+	 * que veremos posteriormente
+	 * 
+	 * IMPORTANTE !!!: una clase bien diseñada es aquella que tiene todas sus
+	 * propiedades o campos privados y solamente se relaciona con el mundo exterior
+	 * a traves de metodos publicos que acceden a dichas variables privadas
+	 */
+
 	
-	/*A continucion las variables mienbros de la clase, que se le llama:
-	 * propiadades, campos y atributos  */
+
+	private String nombre;
+	private String primerApellido;
+	private String segundoApellido;
 	
-	String nombre;
-	String primerApellido;
-	String segundoApellido;
+	/*
+	 * Metodos Getter and Setter, son metodos publicos a traves de los se pueden
+	 * modificar las variables privadas de la clase
+	 */
 	
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getPrimerApellido() {
+		return primerApellido;
+	}
+
+	public void setPrimerApellido(String primerApellido) {
+		this.primerApellido = primerApellido;
+	}
+
+	public String getSegundoApellido() {
+		return segundoApellido;
+	}
+
+	public void setSegundoApellido(String segundoApellido) {
+		this.segundoApellido = segundoApellido;
+	}
+
 }
