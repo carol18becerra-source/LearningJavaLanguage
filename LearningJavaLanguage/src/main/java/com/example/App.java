f361c30613fed7138cf9aa57585a1bd3c221f98c
Carolina <carol18becerra@gmail.com>
2026-04-09 11:34:34 +0200

    Antes de crear rama para utilzar Lombok

--- LearningJavaLanguage/src/main/java/com/example/App.java
+++ LearningJavaLanguage/src/main/java/com/example/App.java
@@ -4,10 +4,24 @@ public class App {
 	public static void main(String[] args) {
 
 		Persona persona1 = new Persona();
-
+		Persona persona2 = new Persona();
+		
+		
 		persona1.setNombre("Dani");
+		
+		
 		
 		System.out.println("el valor de la variable" + "nombre para la referencia es: "
 		+ persona1.getNombre());
+		
+		var persona3 = new Persona("Duglas","Glez","Villamizar");
+		
+		var persona4 = new Persona("Carolina");
+		
+		// Queremos imprimir en la consola persona3
+		
+		System.out.println(persona3.toString());
+		
+		var persona5 = new Persona("3500.50");
 	}
 }
--- LearningJavaLanguage/src/main/java/com/example/Persona.java
+++ LearningJavaLanguage/src/main/java/com/example/Persona.java
@@ -1,6 +1,6 @@
 package com.example;
 
-public class Persona {
+public class Persona extends Object {
 
 	/*
 	 * A continucion las variables mienbros de la clase, que se le llama:
@@ -21,20 +21,70 @@ public class Persona {
 	 * a traves de metodos publicos que acceden a dichas variables privadas
 	 */
 
-	
 
 	private String nombre;
 	private String primerApellido;
 	private String segundoApellido;
+	private double salario;
 	
+	/*Constructor
+	 * 
+	 * ¿Que es un constructor?: Es un metodo (Funcion que esta declarada dentro de la clase) que tiene el mismo 
+	 * nombre de la clase, y que se utiliza conjuntamente con el operardor NEW para construir, instanciar, objetos
+	 * 
+	 * Por defecto si no te creas ningun otro constructor, la clase tiene disponible
+	 *  el llamado constructor sin parametros 
+	 *  
+	 * ¿Como es capaz dicho constructor sin parametros de construir un objeto 
+	 * si en el cuerpo de dicho constructor no hay nada de codigo 
+	 * 
+	 * La respuesta es: Que todas las clases heredan de la clase object, que es la madre de todas las clases
+	 * y por tanto el constructor sin parametros lo que hace es invocar al constructor de la clase object para que le haga 
+	 * los deberes, es decir, le construya el objeto, que la propia clase no tiene ni idea de como hacerlo 
+	 * */
+	
+	public Persona(String nombre, String primerApellido, String segundoApellido) {
+		super();
+		this.nombre = nombre;
+		this.primerApellido = primerApellido;
+		this.segundoApellido = segundoApellido;
+	}
+
+	public Persona(String nombre) {
+		super();
+		this.nombre = nombre;
+	}
+
+	public Persona(String nombre, String primerApellido) {
+		super();
+		this.nombre = nombre;
+		this.primerApellido = primerApellido;
+	}
+
+	public Persona() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
+	public Persona(double salario) {
+		super();
+		this.salario = salario;
+	}
+
 	/*
 	 * Metodos Getter and Setter, son metodos publicos a traves de los se pueden
 	 * modificar las variables privadas de la clase
 	 */
 	
+
+
 	public String getNombre() {
 		return nombre;
 	}
+
+
 
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
@@ -56,4 +106,13 @@ public class Persona {
 		this.segundoApellido = segundoApellido;
 	}
 
+	@Override
+	public String toString() {
+		return "Persona [nombre=" + nombre + ", primerApellido=" + primerApellido + ", segundoApellido="
+				+ segundoApellido + "]";
+	}
+
+	
+	
+	
 }
